d885e63f467aea13a79b69a4dce9121e02260651
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-01 14:02:44 +0200

    Frontend hello world

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'static/index.html'),
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
